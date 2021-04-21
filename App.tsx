95c2333c5bcde3b8ed809b6661d5cadb7f38c795
Paulo Carvalho <pauloej.carvalho@gmail.com>
2021-04-21 16:20:30 -0300

    feat(app): adding external fonts and loading page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import {
+   useFonts,
+   Jost_400Regular,
+   Jost_600SemiBold 
+ } from '@expo-google-fonts/jost';
+
 import { Welcome } from './src/pages/Welcome';
 
+
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if (!fontsLoaded) 
+    return <AppLoading />
+
   return (
     <Welcome />
   )
