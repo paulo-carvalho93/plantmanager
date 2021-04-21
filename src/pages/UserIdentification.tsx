2f1d5ea741f9ebbdee1817090aefb50d6f663c6a
Paulo Carvalho <pauloej.carvalho@gmail.com>
2021-04-21 16:53:21 -0300

    feat(button): creating Button component

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -4,26 +4,46 @@ import {
   View,
   Text,
   TextInput,
-  StyleSheet
+  KeyboardAvoidingView,
+  StyleSheet,
+  Platform
 } from 'react-native';
+
+import { Button } from '../components/Button';
+
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function UserIdentification() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <View style={styles.form}>
-          <Text style={styles.emoji}>
-            😄
-          </Text>
-          <Text style={styles.title}>
-            Como podemos {'\n'}
-            chamar você?
-          </Text>
-          <TextInput style={styles.input} />
+      <KeyboardAvoidingView 
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <View style={styles.header}>
+              <Text style={styles.emoji}>
+                😄
+              </Text>
+              <Text style={styles.title}>
+                Como podemos {'\n'}
+                chamar você?
+              </Text>
+            </View>
+
+            <TextInput 
+              style={styles.input}
+              placeholder="Digite um nome"
+            />
+
+            <View style={styles.footer}>
+              <Button />
+            </View>
+          </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
@@ -43,6 +63,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 54,
+    alignItems: 'center'
+  },
+  header: {
     alignItems: 'center'
   },
   emoji: {
@@ -65,5 +88,10 @@ const styles = StyleSheet.create({
     marginTop: 50,
     padding: 10,
     textAlign: 'center'
+  },
+  footer: {
+    width: '100%',
+    marginTop: 40,
+    paddingHorizontal: 20
   }
 })
