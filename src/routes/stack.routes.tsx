d797e66f924d1ad8318f44d294df3c5c0660cf0f
Paulo Carvalho <pauloej.carvalho@gmail.com>
2021-04-23 20:43:18 -0300

    feat(tabs): creating menu tabs for My Plants and New Plants

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,11 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PlantSave';
 
 import colors from '../styles/colors';
-import { MyPlants } from '../pages/MyPlants';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -38,7 +37,7 @@ const AppRoutes: React.FC = () => (
 
     <stackRoutes.Screen 
       name="PlantSelect"
-      component={PlantSelect}
+      component={AuthRoutes}
     />
 
     <stackRoutes.Screen 
@@ -48,7 +47,7 @@ const AppRoutes: React.FC = () => (
 
     <stackRoutes.Screen 
       name="MyPlants"
-      component={MyPlants}
+      component={AuthRoutes}
     />
   </stackRoutes.Navigator>
 )
